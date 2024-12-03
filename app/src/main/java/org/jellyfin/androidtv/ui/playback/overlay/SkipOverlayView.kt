bd26cc4816e531c511b5a3e569e027dbea14f81e
Brandon Fergerson <bfergerson@apache.org>
2024-12-03 16:51:47 -0300

    wip

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/overlay/SkipOverlayView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/overlay/SkipOverlayView.kt
@@ -27,10 +27,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Text
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.datetime.Duration
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.ui.playback.segment.MediaSegmentRepository
+import kotlinx.datetime.Duration
+import kotlinx.datetime.Duration.Companion.milliseconds
 
 @Composable
 fun SkipOverlayComposable(
